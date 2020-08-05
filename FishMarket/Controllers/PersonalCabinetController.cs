84a19413cb6ab236074d6475bcc7976307ea4321
gravictor <56508699+gravictor@users.noreply.github.com>
2020-08-05 03:05:04 +0300

    Fix moments, admin pannel in personal cabinet, other

--- FishMarket/Controllers/PersonalCabinetController.cs
+++ FishMarket/Controllers/PersonalCabinetController.cs
@@ -19,13 +19,11 @@ namespace FishMarket.Controllers
             db = context;
             _userManager = userManager;
         }
-        public IActionResult Index()
+
+        public IActionResult Index() => View(_userManager.Users.ToList());
+        public async Task<IActionResult> ChangePassword(string id)
         {
-            return View();
-        }
-        public async Task<IActionResult> ChangePassword(string Email)
-        {
-            User user = await _userManager.FindByEmailAsync(Email);
+            User user = await _userManager.FindByIdAsync(id);
             if (user == null)
             {
                 return NotFound();
@@ -33,7 +31,49 @@ namespace FishMarket.Controllers
             ChangePasswordViewModel model = new ChangePasswordViewModel { Id = user.Id, Email = user.Email };
             return View(model);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    var _passwordValidator =
+                    HttpContext.RequestServices.GetService(typeof(IPasswordValidator<User>)) as IPasswordValidator<User>;
+                    var _passwordHasher =
+                    HttpContext.RequestServices.GetService(typeof(IPasswordHasher<User>)) as IPasswordHasher<User>;
+
+                    IdentityResult result =
+                    await _passwordValidator.ValidateAsync(_userManager, user, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        user.PasswordHash = _passwordHasher.HashPassword(user, model.NewPassword);
+                        await _userManager.UpdateAsync(user);
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Пользователь не найден");
+                }
+            }
+            return View(model);
+        }
         public IActionResult MyOrders()
+        {
+            var data = db.order.AsNoTracking().ToList();
+            return View(data);
+        }
+        public IActionResult AdminOrder()
         {
             var data = db.order.AsNoTracking().ToList();
             return View(data);
